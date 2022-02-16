5b22b7adbd2b6d581a173093a36a7337af338c59
radoi-razvan <r4zzzr4zzz@gmail.com>
2022-02-16 10:53:57 +0200

    Create Program.cs

---
+++ Mastery/Program.cs
@@ -0,0 +1,117 @@
+using Mastery.Data;
+using Mastery.Models;
+using Mastery.Services;
+using Mastery.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+                options.UseSqlServer(builder.Configuration["Mastery:ConnectionStrings:MasteryContext"]));
+
+builder.Services.AddIdentityCore<ApplicationUser>(options => 
+            {
+                // Password settings
+                options.Password.RequiredUniqueChars = 1;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 6;
+
+                // User settings
+                options.User.RequireUniqueEmail = true;
+                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";       
+            })
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+// Enable Cors
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy("ReactApp",
+//        builder =>
+//        {
+//            builder.WithOrigins("http://localhost:3000");
+//        });
+//});
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("ReactApp",
+        builder =>
+        {
+            builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        });
+});
+
+builder.Services.AddControllers();
+
+// Intilialize services
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<TokenService>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Sets all data protection tokens to expire in 5 hours
+builder.Services.Configure<DataProtectionTokenProviderOptions>(options =>
+{
+    options.TokenLifespan = TimeSpan.FromHours(5);
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                            builder.Configuration["Mastery:JWTSettings:TokenKey"])
+                        )
+                    };
+                });
+builder.Services.AddAuthorization();
+
+// Logging Configuration
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Client configuration
+//app.UseCors(builder => builder
+//  .WithOrigins("http://localhost:3000")
+//  .AllowAnyMethod()
+//  .AllowAnyHeader()
+//  .AllowCredentials()
+//  .SetIsOriginAllowed(host => true));
+app.UseCors(x => x
+       .AllowAnyOrigin()
+       .AllowAnyMethod()
+       .AllowAnyHeader());
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
