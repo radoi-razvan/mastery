afe6eee34d255efc96e5da5b0329c8620a360c4b
radoi-razvan <r4zzzr4zzz@gmail.com>
2022-02-16 10:56:34 +0200

    Implement UserService

---
+++ Mastery/Services/UserService.cs
@@ -0,0 +1,54 @@
+﻿using Mastery.Data;
+using Mastery.DTOs;
+using Mastery.Models;
+using Mastery.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mastery.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserService(ApplicationDbContext db, UserManager<ApplicationUser> userManager)
+        {
+            _db = db;
+            _userManager = userManager;
+        }
+
+        public async Task<ApplicationUser?> GetByUsernameAsync(string username)
+        {
+            return await _db.Users
+                .Where(u => u.UserName == username)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<bool> Register(RegisterDto registerDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = registerDto.Email,
+                Email = registerDto.Email,
+                FirstName = registerDto.FirstName,
+                LastName = registerDto.LastName,
+                AdressTest = registerDto.AddressTest,
+                TestField = registerDto.TestField,
+                PhoneNumber = registerDto.PhoneNumber,
+                PhoneNumberConfirmed = false
+            };
+
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, registerDto.Role);
+                return true;
+            }
+            return false;
+        }
+
+
+    }
+}
