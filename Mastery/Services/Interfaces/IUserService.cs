e80b7e636b14ed64861608b444e86bc4442fcae9
radoi-razvan <r4zzzr4zzz@gmail.com>
2022-02-16 10:56:09 +0200

    Create IUserService.cs

---
+++ Mastery/Services/Interfaces/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using Mastery.DTOs;
+using Mastery.Models;
+
+namespace Mastery.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<bool> Register(RegisterDto registerDto);
+        Task<ApplicationUser?> GetByUsernameAsync(string username);
+    }
+}
