75493a2f6a32ef76768bf05eb2406c2b01437d0a
radoi-razvan <r4zzzr4zzz@gmail.com>
2022-02-16 10:54:34 +0200

    Create ApplicationUser.cs

---
+++ Mastery/Models/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mastery.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Column(TypeName = "varchar(30)")]
+        public string? FirstName { get; set; }
+        [Column(TypeName = "varchar(30)")]
+        public string? LastName { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        public string? AdressTest { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        public string? TestField { get; set; }
+
+    }
+}
