8534d80e6a837d19fbd9eb78eccd2417592c8134
radoi-razvan <r4zzzr4zzz@gmail.com>
2022-02-16 10:54:44 +0200

    Create ApplicationDbContext.cs

---
+++ Mastery/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Mastery.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mastery.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ?ApplicationUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole>().HasData(
+                new IdentityRole { Name = "Admin", NormalizedName = "ADMIN" },
+                new IdentityRole { Name = "Client", NormalizedName = "CLIENT" }
+            );
+        }
+    }
+}
