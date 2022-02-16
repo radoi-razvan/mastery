4b62410177e18a82716829e8eb9c6e4ac818ef1b
radoi-razvan <r4zzzr4zzz@gmail.com>
2022-02-16 10:56:03 +0200

    Create IService.cs

---
+++ Mastery/Services/Interfaces/IService.cs
@@ -0,0 +1,11 @@
+﻿namespace Mastery.Services.Interfaces
+{
+    public interface IService<T>
+    {
+        Task<T> AddAsync(T item);
+        Task RemoveAsync(int id);
+        Task<T> UpdateAsync(T item);
+        Task<T> GetAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+    }
+}
