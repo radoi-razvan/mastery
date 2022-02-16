73866043dc78e5af5672c7016c1efaaac581669a
radoi-razvan <r4zzzr4zzz@gmail.com>
2022-02-16 10:56:06 +0200

    Create ITokenService.cs

---
+++ Mastery/Services/Interfaces/ITokenService.cs
@@ -0,0 +1,11 @@
+﻿using Mastery.Models;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Mastery.Services.Interfaces
+{
+    public interface ITokenService
+    {
+        public Task<string> GenerateToken(ApplicationUser user);
+        public JwtSecurityToken Verify(string jwt);
+    }
+}
