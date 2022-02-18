fd13882f0e8fc851461739d2cd57367a24c2d1e4
radoi-razvan <r4zzzr4zzz@gmail.com>
2022-02-18 10:04:33 +0200

    Update index.js

--- mastery-client/mastery-react-app/src/index.js
+++ mastery-client/mastery-react-app/src/index.js
@@ -1,13 +1,40 @@
 import React, { StrictMode } from "react";
 import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Header } from "./components/shared/Header";
+import { Footer } from "./components/shared/Footer";
+import { Home } from "./components/shared/Home";
+import { Posts } from "./components/Posts";
+import { Post } from "./components/Post";
+import { NotFound } from "./components/shared/NotFound";
+import { Register } from "./components/shared/Register";
+import { Login } from "./components/shared/Login";
+import { Logout } from "./components/shared/Logout";
 
 const App = () => {
-  return "works";
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/posts" element={<Posts />}></Route>
+        <Route path="/posts/:id" element={<Post />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/logout" element={<Logout />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+      <Footer />
+    </>
+  );
 };
 
 render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
   document.getElementById("root")
 );
