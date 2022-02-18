b90095dc0c76a30355700a54f879e15f3e8f31e6
radoi-razvan <r4zzzr4zzz@gmail.com>
2022-02-18 10:06:10 +0200

    Create NotFound.js

---
+++ mastery-client/mastery-react-app/src/components/shared/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const NotFound = () => {
+  return (
+    <main className="container text-danger">
+      <h1>404</h1>
+      <p>Not Found</p>
+    </main>
+  );
+};
