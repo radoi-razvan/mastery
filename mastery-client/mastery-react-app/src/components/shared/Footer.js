f023d0116c06862815ec97a22b8f852c8dbcba1e
radoi-razvan <r4zzzr4zzz@gmail.com>
2022-03-01 23:01:40 +0200

    Made footer stick to bottom of the page

--- mastery-client/mastery-react-app/src/components/shared/Footer.js
+++ mastery-client/mastery-react-app/src/components/shared/Footer.js
@@ -4,8 +4,8 @@ export const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
-    <footer className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container">
+    <footer className="footer mt-auto navbar navbar-expand-lg bg-dark text-light">
+      <div className="container justify-content-center">
         <span className="navbar-brand">&copy; {year} - Mastery</span>
       </div>
     </footer>
