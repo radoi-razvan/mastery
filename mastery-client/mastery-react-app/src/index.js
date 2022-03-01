001ba6d7620425ab3755e13d96ba676c447a4d36
radoi-razvan <r4zzzr4zzz@gmail.com>
2022-03-01 22:57:53 +0200

    Delete logout component

--- mastery-client/mastery-react-app/src/index.js
+++ mastery-client/mastery-react-app/src/index.js
@@ -1,3 +1,4 @@
+import "./css/App.css";
 import React, { StrictMode } from "react";
 import { render } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -10,7 +11,6 @@ import { Post } from "./components/Post";
 import { NotFound } from "./components/shared/NotFound";
 import { Register } from "./components/shared/Register";
 import { Login } from "./components/shared/Login";
-import { Logout } from "./components/shared/Logout";
 
 const App = () => {
   return (
@@ -22,7 +22,6 @@ const App = () => {
         <Route path="/posts/:id" element={<Post />}></Route>
         <Route path="/register" element={<Register />}></Route>
         <Route path="/login" element={<Login />}></Route>
-        <Route path="/logout" element={<Logout />}></Route>
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
       <Footer />
