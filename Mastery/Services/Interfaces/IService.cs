840bf1e2e6c195b1e6ac2593eda289cb0b9bd17a
radoi-razvan <r4zzzr4zzz@gmail.com>
2022-03-03 21:12:46 +0200

    Update IService.cs

--- Mastery/Services/Interfaces/IService.cs
+++ Mastery/Services/Interfaces/IService.cs
@@ -2,9 +2,9 @@
 {
     public interface IService<T>
     {
-        Task<T> AddAsync(T item);
+        Task AddAsync(T item);
         Task RemoveAsync(int id);
-        Task<T> UpdateAsync(T item);
+        Task UpdateAsync(int id, T item);
         Task<T> GetAsync(int id);
         Task<IEnumerable<T>> GetAllAsync();
     }
