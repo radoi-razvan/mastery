d66bb2cbbf41efac32310bacac998d7f6a803e8c
radoi-razvan <r4zzzr4zzz@gmail.com>
2022-03-15 20:56:01 +0200

    Update color design

--- mastery-client/mastery-react-app/src/components/shared/Footer.js
+++ mastery-client/mastery-react-app/src/components/shared/Footer.js
@@ -4,7 +4,7 @@ export const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
-    <footer className="footer mt-auto navbar navbar-expand-lg bg-dark text-light nav-bg-color">
+    <footer className="footer mt-auto navbar navbar-expand-lg bg-dark text-light nav-bg-color footer-height">
       <div className="container justify-content-center">
         <span className="navbar-brand app-logo">&copy; {year} - Mastery</span>
       </div>
